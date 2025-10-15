e0f6d88af46d266bce4ef395ac933cc2412f07a9
Pedro Hammes <predohammes@gmail.com>
2025-10-15 10:26:58 -0300

    configuração básica do cronometro (start, pause)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const [isRunning, setIsRunning] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+
+  // Efeito que controla o cronômetro
+  useEffect(() => {
+    let intervalId: number | NodeJS.Timeout;
+
+    if (isRunning) {
+      //Começa a contar cada segundo
+      intervalId = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1)
+      }, 1000)
+    }
+
+    //Função de limpeza: para o cronômetro de isRunning virar false
+    return() => {
+      clearInterval(intervalId)
+    }
+
+  }, [isRunning]) // Dependência: o efeito roda quando isRunning muda
+
 
   return (
-    <button onClick={() => setIsRunning(!isRunning)}>
-      {isRunning ? "Pausar" : "Iniciar"}
-    </button>
+    <>
+      <h1>
+        {seconds}
+      </h1>
+      <button onClick={() => setIsRunning(!isRunning)}>
+        {isRunning ? "Pause" : "Start"}
+      </button>
+    </>
   );
 }
