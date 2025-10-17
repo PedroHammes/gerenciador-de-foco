42b58aa802e8b69f65e24a8d810ef64a3b7f8cd1
Pedro Hammes <predohammes@gmail.com>
2025-10-17 15:30:14 -0300

    Implementação do backend da página de cadastro de usuário

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import { useRouter } from "next/router"
+
+export default function Signup() {
+
+        const [email, setEmail] = useState('')
+        const [password, setPassword] = useState('')
+        const [error, setError] = useState('')
+        const router = useRouter()
+
+        // Função para registrar um novo usuário no DB
+        async function Register(event) {
+                event.preventDefault()    
+
+                const response = await fetch('/api/auth/signup', {
+                        method: "POST",
+                        headers: {
+                                'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({email, password}) 
+                })
+                
+                if (response.ok) {
+                        // Direcione para o login se deu certo
+                        router.push('/login')
+
+                } else {
+                       // Responda algo se deu errado 
+                       const errorData = await response.json()
+                       setError(errorData.message)
+                }
+        }
+
+        return (
+                <form onSubmit={Register}>
+                        <input type="email" 
+                        value={email}
+                        onChange={(event) => { 
+                                setEmail(event.target.value)
+                        }}
+                        />
+
+                        <input type="password"
+                        value={password}
+                        onChange={(event) => { 
+                                setPassword(event.target.value)
+                        }}
+                        />
+
+                        <button type="submit">Registrar</button>
+                        <p>
+                                {error}
+                        </p>
+                </form>
+        )
+}
